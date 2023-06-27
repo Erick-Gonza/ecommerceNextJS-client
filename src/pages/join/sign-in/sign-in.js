e9435b6d868ae35b4607506ff84810ec37800c4a
Erick Gzz <eagm080897@gmail.com>
2023-06-27 17:27:33 -0600

    setup config for next and scss

---
+++ src/pages/join/sign-in/sign-in.js
@@ -0,0 +1,11 @@
+import styles from "./sign-in.module.scss";
+
+export default function SignInPage() {
+  return (
+    <>
+      <div>
+        <h3>Log in</h3>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/join/sign-up/sign-up.js
@@ -0,0 +1,11 @@
+import styles from "./sign-up.module.scss";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <div>
+        <h3>Create Account</h3>
+      </div>
+    </>
+  );
+}
